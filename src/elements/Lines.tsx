d4dc3e9856a59057ce6e1c15ded480cc545103a6
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-29 10:33:56 -0400

    Add lines

--- src/elements/Lines.tsx
+++ src/elements/Lines.tsx
@@ -1,6 +1,10 @@
-export const Lines = () => {
+interface LinesProps {
+    direction: string;
+}
+
+export const Lines = ({ direction }: LinesProps) => {
     return (
-        <div className="flex items-end flex-col gap-4 p-4 absolute w-full bottom-0 right-0">
+        <div className={`flex flex-col gap-4 p-4 absolute w-full bottom-0 ${direction}`}>
             <div className="w-1/2 p-1 bg-zinc-900"></div>
             <div className="w-1/2 p-1 bg-zinc-900"></div>
             <div className="w-1/2 p-1 bg-green-300"></div>
