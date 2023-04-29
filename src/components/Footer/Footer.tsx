640018ee5feda0c541931b3926d16e77b414f7b9
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-29 11:33:32 -0400

    Refactor design footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Lines from "../../elements/Lines";
 
 export const Footer = () => {
     return (
-        <footer className="bg-zinc-950">
+        <footer className="bg-zinc-950 z-10 relative">
             <div className="max-w-screen-xl mx-auto p-4 flex items-center justify-between relative">
                 <p className="flex items-center gap-2 z-10">
                     Desarrollado por
