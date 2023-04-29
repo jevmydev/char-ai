3a2956509889c2242d8b0400de7c1519b69a7168
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-29 11:24:25 -0400

    Add new props

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -1,11 +1,21 @@
 interface InputProps {
+    name?: string;
     type?: string;
     placeholder: string;
     isTextArea?: boolean;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ type, placeholder }: InputProps) => {
-    return <input className="bg-zinc-900 outline-none px-5 py-3 flex items-center gap-4 rounded-md transition-colors duration-300 focus:bg-zinc-800" type={type} placeholder={placeholder} />;
+export const Input = ({ name, type, placeholder, onChange }: InputProps) => {
+    return (
+        <input
+            className="bg-zinc-900 outline-none px-5 py-3 flex items-center gap-4 rounded-md transition-colors duration-300 focus:bg-zinc-800"
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            onChange={onChange}
+        />
+    );
 };
 
 export default Input;
--- src/elements/TextArea.tsx
+++ src/elements/TextArea.tsx
@@ -1,9 +1,18 @@
 interface TextAreaProps {
+    name?: string;
     placeholder: string;
+    onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export const TextArea = ({ placeholder }: TextAreaProps) => {
-    return <textarea className="bg-zinc-900 outline-none px-5 py-3 flex items-center gap-4 rounded-md transition-colors duration-300 focus:bg-zinc-800" placeholder={placeholder}></textarea>;
+export const TextArea = ({ name, placeholder, onChange }: TextAreaProps) => {
+    return (
+        <textarea
+            className="bg-zinc-900 outline-none px-5 py-3 flex items-center gap-4 rounded-md transition-colors duration-300 focus:bg-zinc-800"
+            name={name}
+            placeholder={placeholder}
+            onChange={onChange}
+        ></textarea>
+    );
 };
 
 export default TextArea;
