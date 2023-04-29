825055cbbc6cfd479a53353856e6ce3a661c5657
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-29 10:34:08 -0400

    Fix hashlink route

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -2,19 +2,30 @@ import { Link as RouterLink } from "react-router-dom";
 
 interface LinkProps {
     to: string;
+    isHashLink?: boolean;
     title: string;
     target?: string;
     isActive?: boolean;
     children: React.ReactNode;
 }
 
-export const Link = ({ to, title, target, isActive, children }: LinkProps) => {
-    const activeLinkClass = isActive ? "bg-zinc-900 px-6 py-3 hover:bg-zinc-800" : "text-zinc-300";
+export const Link = ({ to, isHashLink, title, target, isActive, children }: LinkProps) => {
+    const activeLinkClass = isActive ? "bg-zinc-900 px-5 py-3 hover:bg-zinc-800" : "text-zinc-300";
 
-    return (
-        <RouterLink className={`${activeLinkClass} rounded-full transition-colors duration-300 hover:text-zinc-50`} to={to} title={title} target={target} rel="noopener noreferrer">
+    return !isHashLink ? (
+        <RouterLink
+            className={`${activeLinkClass} flex items-center gap-4 rounded-md transition-colors duration-300 hover:text-zinc-50`}
+            to={to}
+            title={title}
+            target={target}
+            rel="noopener noreferrer"
+        >
             {children}
         </RouterLink>
+    ) : (
+        <a className={`${activeLinkClass} flex items-center gap-4 rounded-md transition-colors duration-300 hover:text-zinc-50`} href={to} title={title} target={target} rel="noopener noreferrer">
+            {children}
+        </a>
     );
 };
 
