f5fcd697640b7afdd1fb7df7db8bc04d437ef314
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-30 13:33:14 -0400

    Remove optionally image in type

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,7 @@ export interface CharactersType {
     id?: string;
     name: string;
     description: string;
-    image?: string;
+    image: string;
 }
 
 export type CharactersArrayType = Array<CharactersType>;
