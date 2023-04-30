6c4b9246c5714b4e32a689c64699efa81c996093
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-30 13:33:32 -0400

    Change h3 tag content

--- src/components/Create/CreatePreview.tsx
+++ src/components/Create/CreatePreview.tsx
@@ -9,7 +9,7 @@ interface CreatePreviewProps {
 export const CreatePreview = ({ character }: CreatePreviewProps) => {
     return (
         <div className="flex-1 w-full p-4 bg-zinc-950 rounded-md flex flex-col gap-4">
-            <h3 className="font-bold text-2xl">Previsualización</h3>
+            <h3 className="font-bold text-2xl">Previsualización del personaje elegido</h3>
             <CharactersItem character={character} />
         </div>
     );
