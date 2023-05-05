cbd5e0449754c465e1f9e606543bfd61312d4c74
Jeremy Díaz <jeremy664k@gmail.com>
2023-05-05 12:25:36 -0400

    Add route for mocks

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const CHARACTERS_URL = "./src/mocks/characters.json" as const; // will be replaced by the API
+export const CHARACTERS_URL = "/mocks/characters.json" as const; // will be replaced by the API
 export const OPENAI_URL = "https://api.openai.com/v1/chat/completions" as const;
